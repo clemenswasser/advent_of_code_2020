681e04e7a43bde9a700efd371b3b7c362e7f5108
Clemens Wasser <clemens.wasser@gmail.com>
2020-12-02 08:49:36 +0100

    Day Two

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(str_split_once)]
+
 fn day_one() {
     let numbers = std::fs::read_to_string("input/one.txt")
         .unwrap()
@@ -26,6 +28,73 @@ fn day_one() {
     }
 }
 
+fn day_two() {
+    let file_string = std::fs::read_to_string("input/two.txt").unwrap();
+    let pairs = file_string
+        .lines()
+        .map(|line| {
+            let (policy, password) = line.split_once(':').unwrap();
+            
+            (policy.trim(), password.trim())
+        })
+        .collect::<Vec<_>>();
+
+    let valid_passwords_old_policy = pairs
+        .iter()
+        .filter(|(policy, password)| {
+            let (range, char) = policy.split_once(' ').unwrap();
+
+            let (min, max) = {
+                let (min_s, max_s) = range.split_once('-').unwrap();
+                (
+                    min_s.parse::<usize>().unwrap(),
+                    max_s.parse::<usize>().unwrap(),
+                )
+            };
+
+            let char_count = password
+                .chars()
+                .filter(|c| *c == char.chars().next().unwrap())
+                .count();
+
+            char_count >= min && char_count <= max
+        })
+        .count();
+
+    println!("valid_passwords_old_policy: {}", valid_passwords_old_policy);
+
+    let valid_passwords_new_policy = pairs
+        .iter()
+        .filter(|(policy, password)| {
+            let (range, char) = {
+                let (range_s, char_s) = policy.split_once(' ').unwrap();
+                (range_s, char_s.chars().next().unwrap())
+            };
+
+            let (pos1, pos2) = {
+                let (min_s, max_s) = range.split_once('-').unwrap();
+                (
+                    min_s.parse::<usize>().unwrap(),
+                    max_s.parse::<usize>().unwrap(),
+                )
+            };
+
+            password.chars().nth(pos1 - 1).unwrap() == char
+                && password.chars().nth(pos2 - 1).unwrap() != char
+                || password.chars().nth(pos1 - 1).unwrap() != char
+                    && password.chars().nth(pos2 - 1).unwrap() == char
+        })
+        .count();
+
+    println!("valid_passwords_new_policy: {}", valid_passwords_new_policy);
+}
+
 fn main() {
+    println!("Day One:");
     day_one();
+    println!();
+
+    println!("Day Two:");
+    day_two();
+    println!();
 }
